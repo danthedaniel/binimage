fb96c73595bf3da8558c20449d5c98b596a62d70
teaearlgraycold <supacoofoo@gmail.com>
2018-10-28 04:24:15 -0700

    Little cleanin' up

--- src/main.rs
+++ src/main.rs
@@ -85,13 +85,13 @@ fn image_shape(
             let height = int_ceil(num_pixels, width);
             Ok((width, height))
         }
-        (Some(x), None) => {
-            let height = int_ceil(num_pixels, x);
-            Ok((x, height))
+        (Some(width), None) => {
+            let height = int_ceil(num_pixels, width);
+            Ok((width, height))
         }
-        (None, Some(y)) => {
-            let width = int_ceil(num_pixels, y);
-            Ok((width, y))
+        (None, Some(height)) => {
+            let width = int_ceil(num_pixels, height);
+            Ok((width, height))
         }
         _ => unreachable!(),
     }
@@ -103,13 +103,13 @@ fn image_shape(
 /// `buffer_size` - The file size
 /// `arg_shape` - The shape of the output image
 /// `colortype` - The type of pixel to use
-fn bytes_to_add(buffer_size: usize, dims: (u32, u32), colortype: ColorType) -> u32 {
+fn bytes_to_add(buffer_size: usize, dims: (u32, u32), colortype: ColorType) -> usize {
     let bit_depth = colortype.bits_per_pixel();
     let bits_required = dims.0 * dims.1 * bit_depth;
     // Round up a byte if necessary
     let bytes_required = int_ceil(bits_required, 8);
 
-    return bytes_required - buffer_size as u32;
+    return bytes_required as usize - buffer_size;
 }
 
 /// Given a set of CLI arguments, generate an image file from an input file.
@@ -118,30 +118,28 @@ fn bytes_to_add(buffer_size: usize, dims: (u32, u32), colortype: ColorType) -> u
 fn render_file(args: Args) -> Result<(), &'static str> {
     // Read in binary file
     let mut file = File::open(&args.arg_input).map_err(|_| "Couldn't open input file.")?;
-
     let mut buffer: Vec<u8> = Vec::new();
     let file_size = file
         .read_to_end(&mut buffer)
         .map_err(|_| "Couldn't read input file.")?;
 
+    // Figure out image dimensions
     let colortype = ColorType::from_bitdepth(args.flag_bitdepth.unwrap_or(24))?;
     let arg_shape = (args.flag_width, args.flag_height);
     let dims = image_shape(file_size, arg_shape, colortype)?;
-    let size_diff = bytes_to_add(file_size, dims, colortype);
 
-    // Add any extra bytes onto the end as black pixels
-    for _ in 0..size_diff {
-        buffer.push(0);
-    }
+    // Fill in last row of pixels with black if necessary
+    let size_diff = bytes_to_add(file_size, dims, colortype);
+    let mut zeros = vec![0; size_diff];
+    buffer.append(&mut zeros);
 
     // Write image
+    let output_path = args.arg_output.unwrap_or("out.png".to_string());
     image::save_buffer(
-        &args.arg_output.unwrap_or("out.png".to_string()),
+        &output_path,
         &buffer,
         dims.0,
         dims.1,
         colortype.to_image_colortype(),
-    ).unwrap();
-
-    Ok(())
+    ).map_err(|_| "Couldn't save file.")
 }
